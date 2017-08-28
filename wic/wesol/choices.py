30b00be2404ffcb952d73b0b36a43b0e7cfbd052
skawikk <skawikk>
2017-08-28 15:45:57 +0200

    create choices

---
+++ wic/wesol/choices.py
@@ -0,0 +1,26 @@
+ACCOUNT_TYPE_CHOICES = [
+    (1, "Kasa Główna"),
+    (2, "Kasa Fiskalna"),
+    (3, "Konto Bankowe IDEA"),
+    (4, "Konto Bankowe INTELIGO"),
+    (5, "Konto Bankowe NEST"),
+]
+
+PAYMENTS_TYPE_CHOICES = [
+    (1, "Gotówka"),
+    (2, "Przelew"),
+    (3, "Karta płatnicza"),
+]
+# TODO - complete tax rates
+PRODUCTS_TAX_CHOICES = [
+    (1, "A - 23%"),
+    (2, "B - 8%"),
+    (3, "C - ?!"),
+    (4, "D - ?!"),
+    (5, "G - ?!")
+]
+
+PRODUCTS_USED_IN_CHOICES = [
+    (1, "Do sprzedaży"),
+    (2, "Na imprezy"),
+]
