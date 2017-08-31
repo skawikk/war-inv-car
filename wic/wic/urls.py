8861d02208be01056d303ce0567ed173606e090e
skawikk <skawikk>
2017-08-31 18:08:51 +0200

    update settings, urls and migrations

--- wic/wic/urls.py
+++ wic/wic/urls.py
@@ -19,8 +19,14 @@ from django.contrib import admin
 from wesol import views
 
 urlpatterns = [
+    url(r'^$', views.HomeSiteView.as_view(), name="home"),
     url(r'^admin/', admin.site.urls),
+    url(r'^login/$', views.LoginView.as_view(), name="login"),
+    url(r'^logout/$', views.LogoutView.as_view(), name="logout"),
     url(r'^invoice-add/$', views.InvoicesAddNewView.as_view(), name="invoice-add"),
     url(r'^payment-add/$', views.PaymentAddNewView.as_view(), name="payment-add"),
-
+    url(r'^dailyreport/$', views.DailyReportAddNewView.as_view(), name="dailyreport"),
+    url(r'^product-add/$', views.ProductNewAddView.as_view(), name="product-add"),
+    url(r'^products-invoice-add/$', views.ProductsInvoiceAddView.as_view(), name="products-invoice-add"),
+    url(r'^stocktaking/$', views.StocktakingView.as_view(), name="stocktaking"),
 ]
